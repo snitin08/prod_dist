69b65522a982fd03f8e989ab63e4c66c2d5d09c0
snitin08 <nitinpandit.2000@gmail.com>
2020-10-15 00:56:56 +0530

    register all 3 types

--- prod_dist/mainApp/urls.py
+++ prod_dist/mainApp/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from mainApp.views import register_retailer,register_success
+from mainApp.views import register_retailer,register_success, index
+from mainApp import views
 
 app_name = 'mainApp'
 
 urlpatterns = [
-    path('', register_retailer,name='register_retailer'),
+    path('',index,name='index'),
+    path('register/company/', views.register_company,name='register_company'),
+    path('register/distributor/', views.register_distributor,name='register_distributor'),
+    path('register/retailer/', register_retailer,name='register_retailer'),
     path('regiser/success/',register_success,name="register_success"),
 ]
--- prod_dist/mainApp/views.py
+++ prod_dist/mainApp/views.py
@@ -1,9 +1,95 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
+def index(request):
+    if request.session.get('UID')==None:
+        return render(request,'auth/register.html',{})
+
 def register_retailer(request):
     if request.method == 'GET':
         return render(request,'auth/register_retailer.html',{})
+    else:
+        data = request.POST
+        print(data)
+        first_name = data.get('first name')
+        last_name = data.get('Last name')
+        mobile = data.get('Mobile')
+        address = data.get('address')
+        state = data.get('state')
+        city = data.get('city')
+        pincode = data.get('pincode')
+        email = data.get('Email')
+        password = data.get('Password')
+
+        collected_data = {
+            "first_name":first_name,
+            "last_name":last_name,
+            "mobile":mobile,
+            "address":address,
+            "state":state,
+            "city":city,
+            "pincode":pincode,
+            "email":email,
+            "password":password,
+        }
+
+        return redirect('mainApp:register_success')
+
+def register_company(request):
+    if request.method == 'GET':
+        return render(request,'auth/register_company.html',{})
+    else:
+        data = request.POST
+        print(data)
+        company_name = data.get('company name')
+        mobile = data.get('Mobile')
+        address = data.get('address')
+        state = data.get('state')
+        city = data.get('city')
+        pincode = data.get('pincode')
+        email = data.get('Email')
+        password = data.get('Password')
+
+        collected_data = {
+            "company_name":company_name,            
+            "mobile":mobile,
+            "address":address,
+            "state":state,
+            "city":city,
+            "pincode":pincode,
+            "email":email,
+            "password":password,
+        }
+
+        return redirect('mainApp:register_success')
+
+def register_distributor(request):
+    if request.method == 'GET':
+        return render(request,'auth/register_distributor.html',{})
+    else:
+        data = request.POST
+        print(data)
+        first_name = data.get('first name')
+        last_name = data.get('Last name')
+        mobile = data.get('Mobile')
+        address = data.get('address')
+        state = data.get('state')
+        city = data.get('city')
+        pincode = data.get('pincode')
+        email = data.get('Email')
+        password = data.get('Password')
+        collected_data = {
+            "first_name":first_name,
+            "last_name":last_name,
+            "mobile":mobile,
+            "address":address,
+            "state":state,
+            "city":city,
+            "pincode":pincode,
+            "email":email,
+            "password":password,
+        }
+        return redirect('mainApp:register_success')
     
 def register_success(request):
     return HttpResponse("<h1> Register Success!! </h1>")
